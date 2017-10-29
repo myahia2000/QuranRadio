db36ce7145f645c11d0fc11af3091210e270fa71
Mohammed Yahia Hashem <myahia2000@hotmail.com>
2017-10-29 19:58:59 +0200

    adding github link

--- wp-admin/widgets.php
+++ wp-admin/widgets.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8888%2Fwp-admin%2Fwidgets.php&reauth=1">
+		<meta http-equiv="refresh" content="0;url=http://myahia2000.github.io/QuranRadio/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8888%2Fwp-admin%2Fwidgets.php&reauth=1">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8888%2Fwp-admin%2Fwidgets.php&reauth=1";
+			window.location = "http://myahia2000.github.io/QuranRadio/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8888%2Fwp-admin%2Fwidgets.php&reauth=1";
 		</script>
 
-		<p>You are being redirected to <a href="/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8888%2Fwp-admin%2Fwidgets.php&reauth=1">/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8888%2Fwp-admin%2Fwidgets.php&reauth=1</a></p>
+		<p>You are being redirected to <a href="http://myahia2000.github.io/QuranRadio/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8888%2Fwp-admin%2Fwidgets.php&reauth=1">http://myahia2000.github.io/QuranRadio/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8888%2Fwp-admin%2Fwidgets.php&reauth=1</a></p>
 	</body>
 </html>
